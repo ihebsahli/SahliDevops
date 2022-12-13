1be33d479662c20c92a258425331d2187eef41d1
ihebsahli <iheb.sahli@esprit.tn>
2022-12-13 02:19:38 +0100

    commit ihebbv2

--- src/test/java/com/esprit/examen/tests/VoyageurTest.java
+++ src/test/java/com/esprit/examen/tests/VoyageurTest.java
@@ -1,48 +1,73 @@
 package com.esprit.examen.tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.times;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.esprit.examen.entities.Voyageur;
 import com.esprit.examen.repositories.VoyageurRepository;
 import com.esprit.examen.services.VoyageurServiceImpl;
 
-import lombok.extern.slf4j.Slf4j;
 
-@ExtendWith(MockitoExtension.class)
-	@SpringBootTest
-	@Slf4j
-	public class VoyageurTest {
-	    @Mock
-	    VoyageurRepository VoyageurRepository;
-	    @InjectMocks
-	    VoyageurServiceImpl VoyageurService;
-	    
-	    Voyageur voyageur = Voyageur.builder().nomVoyageur("iheb").build();
-
-	    @Test
-	    public void addVoyageur() {
-	        Mockito.when(VoyageurRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(voyageur));
-	        Mockito.when(VoyageurRepository.save(Mockito.any(Voyageur.class))).thenReturn(voyageur);
-	         VoyageurService.ajouterVoyageur(voyageur);
-	        
-	         Voyageur v = VoyageurService.recupererVoyageParId(1L);
-	        		 assertEquals("iheb", v.getNomVoyageur());
-	     
-	        
-	        verify(VoyageurRepository, times(1)).save(Mockito.any(Voyageur.class));
-	        verify(VoyageurRepository, times(2)).findById(Mockito.anyLong());
-	        log.info("ajouterVoyageur test passed");
-	    }
+@RunWith(SpringRunner.class)
+public class VoyageurTest {
+	@InjectMocks
+	static VoyageurServiceImpl voyageurServiceImpl;
+	@Mock
+	static VoyageurRepository voyageurRepository;
+	static List<Voyageur> voyageurs;
+	
+	
+	@BeforeEach
+	 void init() {
+		MockitoAnnotations.openMocks(this);
+		voyageurs =  new ArrayList<>();
+		when(voyageurRepository.findAll()).thenReturn(voyageurs);
 	}
+	
+	@Test
+	void testRetrieveAllVoyageurs(){
+		assertTrue(voyageurServiceImpl.recupererAll().isEmpty());
+		
+		voyageurs.add(mock(Voyageur.class));
+		
+		assertFalse(voyageurServiceImpl.recupererAll().isEmpty());
+		
+	}
+	
+	@Test
+	void testAddVoyageur(){
+		//assertTrue(stockServiceImpl.addStock().isEmpty());
+		voyageurServiceImpl.ajouterVoyageur(mock(Voyageur.class));
+
+		verify(voyageurRepository).save(any());
+		
+
+		
+	}
+	
+	@Test
+	void testUpdateVoyageur(){
+		//assertTrue(stockServiceImpl.addStock().isEmpty());
+		voyageurServiceImpl.modifierVoyageur(mock(Voyageur.class));
+		verify(voyageurRepository).save(any());
+		
+	}
+
+}
